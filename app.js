76d76990b1c8db69777daf1d7192171bfc6ac083
Stephanie Jones <stephanieruthjones@gmail.com>
2022-01-26 10:10:59 -0700

    Add template

---
+++ app.js
@@ -0,0 +1,9 @@
+//create a nav-menu with links to each linksText in arr below
+const linksTextList = ['about', 'menu', 'contact']
+
+const createNavLinks = () => {
+
+}
+createNavLinks()
+
+//use flexbox to style nav (css file)
